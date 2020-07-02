6242eba76839122eff595df618749b8f22b5128c
Niklaus <odingonicolus@gmail.com>
2020-07-02 18:58:39 +0300

    Student Update

---
+++ Student.java
@@ -0,0 +1,12 @@
+class Student {
+
+public static void main (String[] args)
+{int age = 20;
+ String name = "Nicolus Odingo";
+ String studentClass = "First Year";
+
+ System.out.println("Name: "+name);
+ System.out.println("Class: "+studentClass);
+ System.out.println("Age: "+age);
+}
+}
